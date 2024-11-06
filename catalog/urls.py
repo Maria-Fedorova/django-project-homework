8d0c6066a9a5368a30a710203ac867f6b937c315
Maria-Fedorova <157302242+Maria-Fedorova@users.noreply.github.com>
2024-11-06 21:40:22 +0300

    23.2_cash - Clear all migrations and create again

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,7 +11,8 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
-    path("product/<int:pk>", cache_page(60)(ProductDetailView.as_view()), name="product_detail"),
+    #path("product/<int:pk>", cache_page(60)(ProductDetailView.as_view()), name="product_detail"),
+    path("product/<int:pk>", ProductDetailView.as_view(), name="product_detail"),
     path("product/create", ProductCreateView.as_view(), name="product_create"),
     path("product/<int:pk>/update", ProductUpdateView.as_view(), name="product_update"),
     path("product/<int:pk>/delete", ProductDeleteView.as_view(), name="product_delete"),
